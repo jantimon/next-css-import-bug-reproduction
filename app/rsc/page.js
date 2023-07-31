cffe70e9573634fd9c3a2d4d6dda04dfac88f8a3
Jan Nicklas <j.nicklas@me.com>
2023-07-31 08:28:23 +0200

    reproduce-bug

---
+++ app/rsc/page.js
@@ -0,0 +1,5 @@
+import styles from './page.module.css!=!../example.txt'
+
+export default function Home() {
+  return <div className={styles.demo}>Demo</div>
+}
---
+++ app/use-client/page.js
@@ -0,0 +1,6 @@
+"use client"
+import styles from './page.module.css!=!../example.txt'
+
+export default function Home() {
+  return <div className={styles.demo}>Demo</div>
+}
